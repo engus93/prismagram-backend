dc36f05435b0896da3846c496309a351985f7ed0
DuHyeon <engus983@gmail.com>
2019-05-28 14:57:14 +0900

    REFACTORE: #5.6 createAccount Mutation part Two
    Added error console and overlap username

--- origin/src/api/User/createAccount/createAccount.js
+++ origin/src/api/User/createAccount/createAccount.js
@@ -2,8 +2,12 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    createAccount: async (_, args, { request }) => {
+    createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("This username is already taken");
+      }
       await prisma.createUser({
         username,
         email,
