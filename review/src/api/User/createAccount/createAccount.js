837d7974d68ab676aa73e37b6db3fdd24ed4a1f8
DuHyeon <engus983@gmail.com>
2019-06-06 16:05:20 +0900

    FIX: Edited datamodel and resolver

--- review/src/api/User/createAccount/createAccount.js
+++ review/src/api/User/createAccount/createAccount.js
@@ -4,13 +4,25 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      return await prisma.createUser({
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            username
+          },
+          { email }
+        ]
+      });
+      if (exists) {
+        throw Error("This username / email is already taken");
+      }
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio
       });
+      return true;
     }
   }
 };
